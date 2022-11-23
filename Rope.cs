fe5d7c9b8be128182487b50e242e23ef6407b40e
Griffin-Liv <97925185+Griffin-Liv@users.noreply.github.com>
2022-11-23 12:22:19 -0500

    Update Rope.cs

--- Rope.cs
+++ Rope.cs
@@ -1,7 +1,38 @@
-class Node{
+using System;
+
+public class Node{
+    
     private string s;
     private int length;
     private Node left, right;
+
+    public string S{
+        get{return this.s;}
+        set{this.s = value;}
+    }
+
+    public int Length{
+        get{return this.length;}
+        set{this.length = value;}
+    }
+
+    public Node Left{
+        get{return this.left;}
+        set{this.left = value;}
+    }
+
+    public Node Right{
+        get{return this.right;}
+        set{this.right = value;}
+    }
+
+
+    public Node()
+    {
+        this.s = "";
+        this.length = 0;
+    }
+    
 }
 
 
@@ -12,25 +43,51 @@ class Rope{
     public Rope(){}
 
 
-    public Rope(string S){
-        Node curr_node = (ref)this.root;
-        Node prev_node;
-
-        this.root.length = S.length;
-        
-        while(RopeRecurssive(curr_node, S)){
-            
+    public Rope(string str)
+    {
+        if(str.Length > 6){
+            RecursiveConstructor(str, this.root);
         }
-        
-
+        else{
+            this.root.S = str;
+            this.root.Length = str.Length;
+        }
     }
 
 
-    private bool RopeRecurssive(Node node, string s){
+    private void RecursiveConstructor(string str, Node current)
+    {
+        current.Left = new Node();
+        current.Right = new Node();
+
+        string left_half = str.Substring(0, Convert.ToInt32((str.Length / 2)) + (str.Length % 2));
+        string right_half = str.Substring(Convert.ToInt32((str.Length / 2)) + (str.Length % 2), Convert.ToInt32((str.Length / 2)));
+
+        current.Length = str.Length;
+
         
+        current.Left.Length = left_half.Length;
+        current.Right.Length = right_half.Length;
+
+        if(left_half.Length > 6){
+            RecursiveConstructor(left_half, current.Left);
+            RecursiveConstructor(right_half, current.Right);
+        }
+        else{
+            current.Left.S = left_half;
+            current.Right.S = right_half;
+        }
+
     }
 
     public Rope Concatenate(Rope R1, Rope R2){
+        Rope rope = new Rope();
+        
+        rope.root.Length = R1.Length() + R2.Length();
+        rope.root.Left = R1.root;
+        rope.root.Right = R2.root;
+
+        return rope;
 
     }
 
@@ -71,7 +128,7 @@ class Rope{
 
 
     public int Length(){
-        return this.root.length;
+        return this.root.Length;
     }
 
 
